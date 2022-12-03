7ad13b999477d385bf6b6e8f9d821630acc44079
AChenAC <94329383+AChenAC@users.noreply.github.com>
2022-12-02 20:17:36 -0500

    Updated DESCRIPTION

---
+++ R/MCMC.R
@@ -0,0 +1,22 @@
+#' MCMC
+#'
+#' MCMC is used to
+#'
+#' @param model Regression model.
+#'
+#' @param prior Prior functions.
+#'
+#' @param likelihood Log likelihood function.
+#'
+#' @param X Sets of predictors.
+#'
+#' @param y Outcome.
+#'
+#' @return MCMC returns the samples of coefficients.
+#'
+#' @examples
+#'
+#'
+#' @export
+#'
+
