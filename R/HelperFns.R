dcab65755f7796c9826c3104f99eb5ef2b0da29e
AChenAC <94329383+AChenAC@users.noreply.github.com>
2022-12-02 21:06:59 -0500

    Added draft of MCMC and corresponding helper functions.

--- R/HelperFns.R
+++ R/HelperFns.R
@@ -4,9 +4,9 @@
 #'
 #' @param x Regression Coefficients
 #'
-#' @param mu Mean
+#' @param mu Mean. The default value is 0 if not specified.
 #'
-#' @param sigma Standard Deviation
+#' @param sigma Standard Deviation. The default value is 1 if not specified.
 #'
 #' @return GaussianLogPrior returns gaussian prior in log scale.
 #'
@@ -16,7 +16,7 @@
 #'
 #' @export
 #'
-GaussianLogPrior <- function(x, mu, sigma){
+GaussianLogPrior <- function(x, mu = 0, sigma = 1){
   return(sum(-0.5*(x - mu)^2 / sigma^2))
 }
 
@@ -43,3 +43,55 @@ LaplaceLogPrior <- function(x, mu, b){
   return(sum(-abs(x - mu)/b))
 }
 
+#' LogisticModel
+#'
+#' LogisticModel is the mean function (also known as sigmoid function) of logistic regression.
+#'
+#' @param X Sets of predictors in matrix form.
+#'
+#' @param b Beta Coefficients in matrix form.
+#'
+#' @return LogisticModel returns the log version of the mean function of logistic regression.
+#'
+#' @export
+#'
+LogisticModel <- function(b, X){
+  et = exp(-(cbind(1, X) %*% b))
+  return(log(1 / (1 + et)))
+}
+
+#' LogisticLogLik
+#'
+#' LogisticLogLik produces logistic probability.
+#'
+#' @param X Sets of predictors in matrix form.
+#'
+#' @param y outcome variable in matrix form.
+#'
+#' @param b Beta Coefficients in matrix form.
+#'
+#' @return LogisticLogLik returns the logistic log likelihood.
+#'
+#' @export
+#'
+LogisticLogLik <- function(b, X, y) { # logistic probability p(X, y | b)
+  return((sum(LogisticModel(b, X[y,])) + sum(1-LogisticModel(b, X[!y,]))))
+}
+
+#' logPosteriorFunction
+#'
+#' logPosteriorFunction produces un-normalized posterior pdf
+#'
+#' @param X Sets of predictors in matrix form.
+#'
+#' @param y outcome variable in matrix form.
+#'
+#' @param b Beta Coefficients in matrix form.
+#'
+#' @return logPosteriorFunction returns un-normalized posterior pdf.
+#'
+#' @export
+#'
+logPosteriorFunction <- function(b, X, y){ # un-normalized posterior pdf
+  return(logPriorFunction(b, X, y) + logLikelihoodFunction(b, X, y)) # return scalar p(b | X, y)
+}
--- R/MCMC.R
+++ R/MCMC.R
@@ -1,6 +1,31 @@
-#' MCMC
+logisticModel <- function(b, X){
+  if(length(X)>0){
+    if(nrow(t(X))==1){
+      X = matrix(X,nrow=1)
+    }
+    et = exp(-(cbind(rep(1, dim(X)[1]), X) %*% b))
+  }
+  else{
+    et = 0
+  }
+  return(log(1 / (1 + et)))
+}
+
+logPriorFunction <- function(x, mu=0, sigma=1){ # p(b)
+  return(sum(-0.5*(x - mu)^2 / sigma^2))
+}
+
+logLikelihoodFunction <- function(b, X, y) { # logistic probability p(X, y | b)
+  return((sum(logisticModel(b, X[y==1,])) + sum(1-logisticModel(b, X[y!=1,]))))
+}
+
+logPosteriorFunction <- function(b, X, y){ # un-normalized posterior pdf
+  return(logPriorFunction(b, X, y) + logLikelihoodFunction(b, X, y)) # return scalar p(b | X, y)
+}
+
+#' MCMC.beta.generate
 #'
-#' MCMC is used to
+#' MCMC.beta.generate is used to generate samples of beta coefficients.
 #'
 #' @param model Regression model.
 #'
@@ -8,15 +33,99 @@
 #'
 #' @param likelihood Log likelihood function.
 #'
+#' @param posterior Posterior function.
+#'
 #' @param X Sets of predictors.
 #'
 #' @param y Outcome.
 #'
-#' @return MCMC returns the samples of coefficients.
-#'
-#' @examples
-#'
+#' @return MCMC.beta.generate returns the samples of beta coefficients.
 #'
 #' @export
 #'
+MCMC.beta.generate = function(model, prior, likelihood, posterior, X, y){
+  #data preprocessing
+  X = scale(X) # centered features
+  pca = prcomp(X, center = TRUE,scale. = TRUE) # pca on centered features (not essential, but helpful)
+  X = pca$x[,1:(ncol(X)/2)] # top 1/2 principle component features
+  d = dim(X)[2]
+  #sample initialization
+  n = 100000 # number of mcmc samples
+  n_burnin = n / 10 # 10% burn-in
+  samples = matrix(0, nrow=(n+n_burnin), ncol=d+1) # allocation
+  currentProbability = posterior(samples[1,], X, y) # mcmc p(b | X, y) sample 1
+  proposal_cov_chol = chol(diag(d+1))
+  sd = 2.4^2 / d # scaling factor, see Haario et al 2001
 
+  for(i in 1:(n + n_burnin-1)){ # vanilla MCMC
+
+    if(i==n_burnin){
+      proposal_cov_chol = chol(cov(samples[1:n_burnin,])*sd)
+    }
+    deltaProposal = proposal_cov_chol %*% rnorm(d+1)
+
+    proposal = matrix(samples[i,] + deltaProposal, nrow=d+1)
+    proposalProbability = posterior(proposal, X, y)
+    logAlpha = (proposalProbability - currentProbability) # log alpha = log proposalP - log currentP
+
+    logU = log(runif(1)) # log u
+    acceptance = logAlpha >= logU # bool
+
+    if(is.na(acceptance)){ # if nan
+      samples[i+1,] = rnorm(d+1, 0, 1) # if nan then start chain at random value
+      currentProbability = posterior(samples[i+1,], X, y)
+    }
+
+    else if(acceptance){ # if accept
+      # print(TRUE)
+      samples[i+1,] = proposal
+      currentProbability = proposalProbability
+    }
+
+    else if(!acceptance){ # if reject
+      # print(FALSE)
+      samples[i+1,] = samples[i,]
+    }
+  }
+  sample_stationary = samples[n_burnin+1:(n+n_burnin),]
+  return(sample_stationary)
+}
+
+#' MCMC.prediction
+#'
+#' MCMC.prediction is used to generate predictive outcomes.
+#'
+#' @param model Regression model.
+#'
+#' @param likelihood Log likelihood function.
+#'
+#' @param sample samples of beta coefficients genereated with MCMC.beta.generate.
+#'
+#' @param X Sets of predictors.
+#'
+#' @param selected.clusters Selected cluster labels as a vector.
+#'
+#' @import stats
+#'
+#' @return MCMC.prediction returns predicted outcome with samples of beta coefficients.
+#'
+#' @export
+#'
+MCMC.prediction = function(model, likelihood, sample, X, selected.clusters){
+  n = nrow(X)
+  k = length(selected.clusters)
+  prob_mat = matrix(NA,n,k)
+
+  #preprocessing
+  X = scale(X) # centered features
+  pca = prcomp(X, center = TRUE,scale. = TRUE) # pca on centered features (not essential, but helpful)
+  X = pca$x[,1:(ncol(X)/2)] # top 1/2 principle component features
+
+  for(i in 1:n){
+    for(j in 1:k){
+      prob_mat[i,j] = likelihood(t(t(sample[i,])),t(X[i,]),selected.clusters[j])
+    }
+  }
+  y_pred = selected.clusters[apply(prob_mat,1,which.max)]
+  return(y_pred)
+}
